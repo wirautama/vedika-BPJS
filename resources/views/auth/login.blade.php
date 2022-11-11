534abf9d0446d036ee68b61c13cddb2ae860b149
zunafif <zunafif@gmail.com>
2022-11-11 17:34:17 +0700

    fix login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -72,15 +72,6 @@
           </div>
           <!-- /.col -->
         </div>
-        <div class="row mb-0">
-            <div class="col-md-8 offset-md-4">
-                @if (Route::has('password.request'))
-                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                        {{ __('Forgot Your Password?') }}
-                    </a>
-                @endif
-            </div>
-        </div>
       </form>
     </div>
     <!-- /.login-card-body -->
